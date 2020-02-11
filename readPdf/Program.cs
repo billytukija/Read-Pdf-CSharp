c9ba625aaa69345673716d4476751094027efe15
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-02-11 20:16:56 -0300

    Leitura de Pdf em c#

---
+++ readPdf/Program.cs
@@ -0,0 +1,26 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+
+namespace readPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = "your dir";
+
+            PdfReader reader = new PdfReader(path);
+            string text = string.Empty;
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(reader, page);
+            }
+            
+            reader.Close();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
